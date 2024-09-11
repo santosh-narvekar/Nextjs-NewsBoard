44ae2545bd7722c54f6a0d14ac4a608788097610
Santosh Narvekar <120112762+santosh-narvekar@users.noreply.github.com>
2024-09-12 00:47:07 +0530

    Update search-news.ts

--- pages/api/search-news.ts
+++ pages/api/search-news.ts
@@ -3,10 +3,6 @@
 import { NewsResponse } from "@/models/NewsArticles";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
